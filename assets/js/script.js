c05d8b9b54215583b350c885a54f966316d475e9
Tyler Eppley <teppley300@gmail.com>
2023-04-02 00:00:39 -0400

    added css and js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,90 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById
+('answer-buttons')
+
+let shuffQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+startButton.classList.add('hide')
+shuffQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+nextQuestion()
+
+}
+
+function nextQuestion() {
+  resetState()
+  showQuestion(shuffQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question){
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
 
 
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
 
+function resetState(){
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild){
+    answerButtonsElement.removeChild
+    (answerButtonsElement.firstChild)
+  }
+}
 
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  nextButton.classList.remove('hide')
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')  
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element){
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is a JavaScript element that represents either TRUE or FALSE values?',
+    answers: [
+      {text: 'Function'},
+      {text: 'Event'},
+      {text: 'Boolean'},
+      {text: 'Condition'}
+    
+    ]
+  }
+]
 
 
 
