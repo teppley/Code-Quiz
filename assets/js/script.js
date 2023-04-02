d2558daffcb21b0e7a3bcf6da3e16d56e8935afe
Tyler Eppley <teppley300@gmail.com>
2023-04-01 20:13:31 -0400

    added file structure, pseudo code, and html

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Pseudo Code
+//Create a "Start Quiz" button
+  //Display a timer and highscore on the page 
+//Create a series of questions with four possible answers
+  //Display if the question was answered correct or incorrect
+  //Incorrect answers affect the total time 
+  //User moves through all the questions until they have finished the quiz or the timer reaches zero
+//High scores are displayed and stored in local storage  
+
+// Create new HTML file with reusable elements such as:
+
+// // 1. Question container
+// // 2. Initials container
+// // 3. Timer container
+
+// Create a new JS file with elements such as:
+
+// // 1. Variables to keep track of quiz state
+// // 2. Variables to reference DOM elements/elements to be created
+// // 3. Sound effects (if you wanna get fancy)         
+// // 4. Functions to start quiz, get question, and check answer, control the timer
+// // 5. Function to save high scores to local storage
+
