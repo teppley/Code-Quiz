fc354bb80b36e589199c31721dec95cb59adaaf9
Tyler Eppley <teppley300@gmail.com>
2023-04-04 20:03:26 -0400

    added questions and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ const answerButtonsElement = document.getElementById
 
 let shuffQuestions, currentQuestionIndex
 
+//starting the quiz
 startButton.addEventListener('click', startGame)
 
 function startGame() {
@@ -15,12 +16,44 @@ startButton.classList.add('hide')
 shuffQuestions = questions.sort(() => Math.random() - .5)
 currentQuestionIndex = 0
 questionContainerElement.classList.remove('hide')
-nextQuestion()
-
+showQuestion(shuffQuestions[currentQuestionIndex])
 }
 
+//timer with alert
+let timeLeft = 60;
+let timer = setInterval(() => {
+timeLeft--;
+let minutes = Math.floor(timeLeft / 60);
+let seconds = timeLeft % 60;
+document.getElementById("timer").innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+if (timeLeft === 0) {
+clearInterval(timer);
+alert("Time's up!");
+}
+}, 1000);
+
+
+//Timer
+/*var startTimer
+var timeLeft = 60;
+var elem = document.getElementById('Timer');
+
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft === 0) {
+    clearTimeout(timerId);
+
+  } else {
+    elem.innerHTML = timeLeft + ' seconds remaining';
+    timeLeft--;
+  }
+}*/
+
+//question shuffler
 function nextQuestion() {
   resetState()
+  currentQuestionIndex++
   showQuestion(shuffQuestions[currentQuestionIndex])
 
 }
@@ -42,7 +75,7 @@ function showQuestion(question){
 }
 
 function resetState(){
-  nextButton.classList.add('hide')
+  //nextButton.classList.add('hide')
   while (answerButtonsElement.firstChild){
     answerButtonsElement.removeChild
     (answerButtonsElement.firstChild)
@@ -50,13 +83,14 @@ function resetState(){
 }
 
 function selectAnswer(e) {
+  nextQuestion()
   const selectedButton = e.target
   const correct = selectedButton.dataset.correct
   setStatusClass(document.body, correct)
   Array.from(answerButtonsElement.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
-  nextButton.classList.remove('hide')
+
 }
 
 function setStatusClass(element, correct) {
@@ -73,38 +107,54 @@ function clearStatusClass(element){
   element.classList.remove('wrong')
 }
 
+//question Index
 const questions = [
   {
     question: 'What is a JavaScript element that represents either TRUE or FALSE values?',
     answers: [
-      {text: 'Function'},
-      {text: 'Event'},
-      {text: 'Boolean'},
-      {text: 'Condition'}
-    
+      {text: 'Function', correct:false}, 
+      {text: 'Event', correct:false},
+      {text: 'Boolean', correct: true},
+      {text: 'Condition', correct: false}
     ]
-  }
+  },
+  {
+    question: 'What is the correct HTML element for a line break?',
+    answers: [
+      {text: '<brk>', correct:false},
+      {text: '<break>', correct:false},
+      {text: '<bk>', correct:false},
+      {text: '<br>', correct:true}
+    ]
+  },
+  {
+    question: 'What does Http stand for?',
+    answers: [
+      {text: 'Hyper transfer text protocol', correct:false},
+      {text: 'Hot tamales to prepare', correct:false},
+      {text: 'Hyper text transfer protocol', correct:true},
+      {text: 'Hyper text markup language', correct:false}
+  ]
+},
+{
+    question: 'Creating an object is called?',
+    answers: [
+      {text: 'Instantiation', correct:true},
+      {text: 'Variable', correct:false},
+      {text: 'Reference', correct:false},
+      {text: 'Abstraction', correct:false},
+  ]
+},
+{
+  question: 'In CSS and HTML what colors are displayed by combining these three shades?',
+  answers: [
+    {text: 'black, white, and grey', correct:false},
+    {text: 'green, red, yellow', correct:false},
+    {text: 'red, green, blue', correct:true},
+    {text: 'red, green, black', correct:false}
+  ]
+}
 ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
